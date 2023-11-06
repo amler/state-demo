c40d7e3c9e1b5b9c65bfd9ba4bfd59632347233a
Heather Amler <amler.heather@gmail.com>
2023-11-06 14:28:16 -0500

    Adding common react one liner practices in React for copying an array without mutating

---
+++ src/commonArrayPatterns.js
@@ -0,0 +1,31 @@
+// common Patterns for updating arrays in state
+const shoppingCart = [
+    {id: 1, product: "turkey", price: 4},
+    {id: 2, product: "eggs", price: 28},
+    {id: 3, product: "brownie", price: 6.5},
+];
+
+
+// adding to an array
+[...shoppingCart, {id: 4, product: "coffee", price:7.99}];
+
+// removing an element
+shoppingCart.filter((item) => item.id !== 2);
+
+// updating all elements in an array to change a name for example
+shoppingCart.map((item) => {
+    return {
+        ...item,
+        product: item.product.toLocaleLowerCase, 
+    };
+})
+
+
+// modifying a particular element in an array
+shoppingCart.map((item) => {
+    if (item.id === 3) {
+        return {...item, price: 10.99}
+    } else {
+        return item;
+    }
+});
